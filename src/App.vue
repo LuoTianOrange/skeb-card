34bf0b320ac39e4766fc074c9b2ed81f5a6d36cb
LuoTianOrange <2977277015@qq.com>
2024-11-19 00:39:48 +0800

    删除文件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,3 @@
-<script setup>
-import { onMounted, ref } from 'vue';
-import axios from 'axios'
-
-const username = "@hy_plus"
-
-const data = ref()
-
-onMounted(async () => {
-  try {
-    const response = await axios.get(`/api?username=${username}`);
-    data.value = response.data;
-  } catch (error) {
-    console.error('Error fetching SVG:', error);
-  }
-})
-console.log(data);
-</script>
-
 <template>
   <div>
   </div>
